3fcc88c89352778f25b13007c60cfd725b82d553
Denis Bardadym <bardadymchik@gmail.com>
2016-01-28 23:07:56 +0300

    Add spec of safe json stringify

---
+++ utils/safe-json-stringify.spec.js
@@ -0,0 +1,9 @@
+var stringify = require('./safe-json-stringify');
+
+describe('Safe JSON.stringify', function() {
+  it('should mark property as cycle if was seen before', function() {
+    var a = {b: 10};
+    a.a = a;
+    stringify(a).should.be.equal('{"b":10,"a":"[Circular]"}');
+  });
+});
